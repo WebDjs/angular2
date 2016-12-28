b2467d318a192dd3c58341b673491aa249f5580d
Stefan M. Stefanov <ststefan@progress.com>
2016-12-28 16:04:57 +0200

    adding image gallery for home page

--- src/app/common/home.component.ts
+++ src/app/common/home.component.ts
@@ -1,11 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    animations: [
+        trigger('focusPanel', [
+            state('moveOne', style({
+                marginLeft: '-720px'
+            })),
+            state('moveTwo', style({
+                marginLeft: '-1440px'
+            })),
+            state('moveThree', style({
+                marginLeft: '-2160px'
+            })),
+            state('reset', style({
+                marginLeft: '0px'
+            })),
+            transition('moveOne => moveTwo', animate('1000ms ease-in')),
+            transition('moveTwo => moveThree', animate('1000ms ease-in')),
+            transition('moveThree => reset', animate('1000ms ease-in')),
+            transition('reset => MoveOne', animate('1000ms ease-in')),
+        ])
+    ]
 })
 export class HomeComponent implements OnInit {
     constructor() { }
 
-    ngOnInit() { }
+    state: string = 'moveOne';
+
+    ngOnInit() {
+            setInterval(() => { this.changeSlide(); }, 3000);       
+    }
+
+    changeSlide() {
+        console.log(this.state);
+        if (this.state === 'moveThree') {
+            this.state = 'reset';
+        } 
+
+        if (this.state === 'moveTwo') {          
+            this.state = 'moveThree';
+        } 
+
+        if (this.state === 'moveOne') {
+            this.state = 'moveTwo';
+        } 
+
+        if (this.state === 'reset') {
+            this.state = 'moveOne';
+        } 
+    }
 }
