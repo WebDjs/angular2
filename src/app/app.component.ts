4fe6773870689fac6e1882c2e02253929fc286d5
dtopalov <dimiter.topalov79@gmail.com>
2016-12-31 19:01:48 +0200

    minor style exp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,16 @@ import 'mdl-select-component/mdl-selectfield.min';
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
   styles: [
+    `
+      .page-content {
+        background-color: #f5f5f5;
+        width: 100%;
+        margin: 0 auto;
+        padding: 30px;
+      }
+    `,
     require('material-design-lite/dist/material.min.css'),
-    require('mdl-select-component/mdl-selectfield.min.css') // for select input (optional)
+    require('mdl-select-component/mdl-selectfield.min.css'), // for select input (optional)
   ]
 })
 export class AppComponent implements OnInit, DoCheck {
