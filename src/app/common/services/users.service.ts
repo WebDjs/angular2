59b37631561512779a7e043c94b97f1d8f0959cf
dtopalov <dimiter.topalov79@gmail.com>
2017-01-01 13:04:10 +0200

    moved create user from auth to UsersService

--- src/app/common/services/users.service.ts
+++ src/app/common/services/users.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
 import { User } from '../models/';
 
 @Injectable()
 export class UsersService {
 
-    constructor() { }
+    constructor(private http: Http) { }
+
+    create(user: User) {
+        let headers = new Headers();
+        headers.append('content-type', 'application/json');
+
+        return this.http.post(
+            'http://localhost:3000/api/signup',
+            JSON.stringify(user),
+            { headers: headers })
+            .map((response: Response) => {
+                // register and login successful if there's a jwt token in the response
+                let registeredUser = response.json();
+                if (registeredUser && registeredUser.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(registeredUser));
+                }
+            });
+    }
 
     loggedUser(): User {
         let storedUser = localStorage.getItem('currentUser');
