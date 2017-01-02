f69c1d81a8b5e2c44058ebb00012d59ce985a0da
dtopalov <dimiter.topalov79@gmail.com>
2017-01-02 21:56:46 +0200

    db in mlab

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'someawesomesecret',
-  'database': 'mongodb://localhost:27017/a2teamwork',
-  // 'database': 'mongodb://kendouser:kendopass@ds151208.mlab.com:51208/divings'
+  // 'database': 'mongodb://localhost:27017/a2teamwork',
+  'database': 'mongodb://kendoteam2:kendoparola@ds151208.mlab.com:51208/divings'
 };
