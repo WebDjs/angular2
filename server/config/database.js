169b5b4e405ea49e07dcae4452f26900764e555a
dtopalov <dimiter.topalov79@gmail.com>
2017-01-02 16:01:58 +0200

    server db back to local

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   'secret': 'someawesomesecret',
-  'database': 'mongodb://kendouser:kendopass@ds151208.mlab.com:51208/divings'
+  'database': 'mongodb://localhost:27017/a2teamwork',
+  // 'database': 'mongodb://kendouser:kendopass@ds151208.mlab.com:51208/divings'
 };
