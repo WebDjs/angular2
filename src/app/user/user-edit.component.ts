3ce157c0d4cd1d2964b97305af97bc3d908158ad
Alex-Bubblemaster <shiatzu@gmail.com>
2017-01-03 01:33:13 +0200

    fix: copy-paste errors

--- src/app/user/user-edit.component.ts
+++ src/app/user/user-edit.component.ts
@@ -22,6 +22,7 @@ export class UserEditComponent {
   }
 
   submit(userData: User, isValid: boolean) {
+    console.log(userData ,"submit");
     if (isValid) {
       this._usersService.update(userData).subscribe((response: Response) => {
         this.stateChange.emit("read");
