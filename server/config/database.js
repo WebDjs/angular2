1ea031f4f65bcb6ae7d7468a5a8f1f672f653c90
dtopalov <dimiter.topalov79@gmail.com>
2017-01-02 15:53:46 +0200

    mongolab exp

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'someawesomesecret',
-  'database': 'mongodb://localhost:27017/a2teamwork'
+  'database': 'mongodb://kendouser:kendopass@ds151208.mlab.com:51208/divings'
 };
