062fbf2b9d7fa806bea656f4e99b18b0d94ebc33
dtopalov <dimiter.topalov79@gmail.com>
2017-01-05 22:20:51 +0200

    try 2

--- src/app/common/services/data.service.ts
+++ src/app/common/services/data.service.ts
@@ -45,9 +45,6 @@ export class DataService {
     }
 
     private handleError(error: Response) {
-        // in a real world app, we may send the server to some remote logging infrastructure
-        // instead of just logging it to the console
-        console.error(error);
         return Observable.throw(error.json().error || 'Server error');
     }
 
